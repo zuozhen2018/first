63768d87cb298b41a431d4c3690ae42de7034e0e
lixuepeng@zealfi.com <lixuepeng@zealfi.com>
2018-04-04 13:30:47 +0800

    my test

---
+++ src/test/Servlet2.java
@@ -0,0 +1,39 @@
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @作者:lixuepeng
+ * @日期:2018年04月03日 13:59:22
+ *
+ */
+public class Servlet2 extends HttpServlet {
+	private String message;
+
+	/**
+	 *
+	 * @throws ServletException
+	 */
+	@Override
+	public void init() throws ServletException {
+		message = "Hello world, this message is from servlet!";
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//设置响应内容类型
+		resp.setContentType("text/html");
+		//设置逻辑实现
+		PrintWriter out = resp.getWriter();
+		out.println("<h3>" + message + "</h3>");
+	}
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+}
